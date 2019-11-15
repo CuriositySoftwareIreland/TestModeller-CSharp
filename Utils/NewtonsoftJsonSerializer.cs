1791812bf95be3fbc94032e13747cbbbd60eb815
Andrey Ustalakov <austalakov@gmail.com>
2019-11-15 15:09:36 +0000

    add a generic REST service to inherit from

--- Utils/NewtonsoftJsonSerializer.cs
+++ Utils/NewtonsoftJsonSerializer.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace CuriositySoftware.Utilities
+namespace CuriositySoftware.Utils
 {
     public class NewtonsoftJsonSerializer : ISerializer, IDeserializer
     {
